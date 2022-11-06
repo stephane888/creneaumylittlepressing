b7253723b73551effbb70435042fd16cda925c48
kouwa stephane <kksasteph888@gmail.com>
2022-11-06 20:32:17 +0100

    Maj

--- src/Controller/CreneaumylittlepressingController.php
+++ src/Controller/CreneaumylittlepressingController.php
@@ -3,37 +3,94 @@
 namespace Drupal\creneaumylittlepressing\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\shopify_api_rest\Services\ManageAccessToken;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Returns responses for creneaumylittlepressing routes.
  */
 class CreneaumylittlepressingController extends ControllerBase {
-
+  
+  /**
+   *
+   * @var ManageAccessToken
+   */
+  protected $ManageAccessToken;
+  
+  /**
+   *
+   * @param ManageAccessToken $ManageAccessToken
+   */
+  function __construct(ManageAccessToken $ManageAccessToken) {
+    $this->ManageAccessToken = $ManageAccessToken;
+  }
+  
+  /**
+   *
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('shopify_api_rest.manage_access_token'));
+  }
+  
   /**
    * Builds the response.
+   * 1 : La premiere requete ( Generalement "demande d'installation" ) arrive.
+   * On verifie si le client est deja enregistrée.
+   * 1: 1 si nouveau client: On doit le rediriger vers la page d'authorisation
+   * en precissant l'url de retour.
+   * 1: 2 Shopify redirigera vers la page de redirect avec un
+   * code_dautorisation.( pour determiner si
+   * l'utilisateur a authorisé ou l'application).
+   * 1: 3 On effectue une requete pour avoir un token_access en utilisant le
+   * code_autorisation.
    */
-  public function AppCreneaux() {
-
+  public function AppCreneaux(Request $Request) {
+    $configs = $this->config('creneaumylittlepressing.settings')->getRawData();
+    //
+    $grantOptions = $configs['grant_options'];
+    $this->ManageAccessToken->run($Request, $grantOptions);
     $build['content'] = [
-      '#type' => 'item',
-      '#markup' => $this->t('It works!'),
+      '#theme' => 'Creneaumylittlepressing',
+      '#title' => "Configuration de lapplication creneau",
+      '#configsCreneau' => [],
+      '#attached' => [
+        'library' => [
+          'creneaumylittlepressing/app'
+        ]
+      ]
+    ];
+    $build['content']['#attached']['drupalSettings']['creneaumylittlepressing'] = [
+      'creneau_configs' => [],
+      'creneau_types' => [],
+      "creneau_filters" => []
     ];
     return $build;
   }
-
-  public function shopify(){
+  
+  /**
+   *
+   * @return string[]|\Drupal\Core\StringTranslation\TranslatableMarkup[]
+   */
+  public function shopify() {
     $build['content'] = [
       '#type' => 'item',
-      '#markup' => $this->t('It works!'),
-    ];    
+      '#markup' => $this->t('It works!')
+    ];
     return $build;
   }
   
-  public function Valid(){
+  /**
+   *
+   * @return string[]|\Drupal\Core\StringTranslation\TranslatableMarkup[]
+   */
+  public function Valid() {
     $build['content'] = [
       '#type' => 'item',
-      '#markup' => $this->t('It works!'),
-    ];    
+      '#markup' => $this->t('It works!')
+    ];
     return $build;
   }
+  
 }
--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -4,56 +4,84 @@ namespace Drupal\creneaumylittlepressing\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\shopify_api_rest\ShopifyApiRest;
 
 /**
  * Configure creneaumylittlepressing settings for this site.
  */
 class SettingsForm extends ConfigFormBase {
-
+  
   /**
+   *
    * {@inheritdoc}
    */
   public function getFormId() {
     return 'creneaumylittlepressing_settings';
   }
-
+  
   /**
+   *
    * {@inheritdoc}
    */
   protected function getEditableConfigNames() {
-    return ['creneaumylittlepressing.settings'];
+    return [
+      'creneaumylittlepressing.settings'
+    ];
   }
-
+  
   /**
+   *
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['example'] = [
+    $config = $this->config('creneaumylittlepressing.settings');
+    $form['client_id'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('Example'),
-      '#default_value' => $this->config('creneaumylittlepressing.settings')->get('example'),
+      '#title' => $this->t('ID client'),
+      '#default_value' => $config->get('client_id'),
+      '#required' => true
+    ];
+    $form['client_secret'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Secret client'),
+      '#default_value' => $config->get('client_secret'),
+      '#required' => true
+    ];
+    $form['grant_options'] = [
+      '#type' => 'select',
+      '#title' => $this->t('select grant options'),
+      '#default_value' => $config->get('grant_options'),
+      '#options' => ShopifyApiRest::listGrantOption(),
+      '#required' => true
     ];
     return parent::buildForm($form, $form_state);
   }
-
+  
   /**
+   *
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    if ($form_state->getValue('example') != 'example') {
-      $form_state->setErrorByName('example', $this->t('The value is not correct.'));
-    }
+    // if ($form_state->getValue('example') != 'example') {
+    // $form_state->setErrorByName('example', $this->t('The value is not
+    // correct.'));
+    // }
     parent::validateForm($form, $form_state);
   }
-
+  
   /**
+   *
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $this->config('creneaumylittlepressing.settings')
-      ->set('example', $form_state->getValue('example'))
-      ->save();
+    $editConfig = $this->config('creneaumylittlepressing.settings');
+    //
+    $editConfig->set('client_id', $form_state->getValue('client_id'));
+    $editConfig->set('client_secret', $form_state->getValue('client_secret'));
+    $editConfig->set('grant_options', $form_state->getValue('grant_options'));
+    $editConfig->save();
+    //
     parent::submitForm($form, $form_state);
   }
-
+  
 }
